2c310431c5459e04bcc43a9392382e80fb658f03
Carolyn Whelpley Whelpley <c@carolyns-mbp.lan>
2026-02-26 21:03:08 -0500

    add Board model

--- apps/boards/admin.py
+++ apps/boards/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Board, BoardMembership, Column 
 
 # Register your models here.
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'created_at']
+    search_fields = ['title']
+
+
+@admin.register(BoardMembership)
+class BoardMembershipAdmin(admin.ModelAdmin):
+    list_display = ['user', 'board', 'role', 'joined_at']
+
+
+@admin.register(Column)
+class ColumnAdmin(admin.ModelAdmin):
+    list_display = ['title', 'board', 'position']
+    ordering = ['board', 'position']
